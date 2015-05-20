01468461dfe5ed40058d79c419b9f3cbd2a900a6
AL07F13 <ceronherrey@gmail.com>
2015-05-20 13:43:28 -0500

    aplicacion_juego
    archivo de un juego

---
+++ js/acciones.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+$(document).ready(function(e) {
+//document.addEventListener("deviceready",function(){
+	{
+		$('#uno').on('click', function(){
+			$('#colores').append('1')
+		});
+		$('#dos').on('click', function(){
+			$('#colores').append('2')
+		});
+		$('#tres').on('click', function(){
+			$('#colores').append('3')
+		});
+		$('#cuatro').on('click', function(){
+			$('#colores').append('4')
+		});
+	}
+//}); 
+});
+
